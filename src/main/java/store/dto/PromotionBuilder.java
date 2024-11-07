13375358aeb904a45aa70d28e23854eda07ed836
minseo <alstj778@gmail.com>
2024-11-07 16:54:47 +0900

    fix(Promotion): 날짜 데이터 타입 LocalDate 로 변경

--- src/main/java/store/dto/PromotionBuilder.java
+++ src/main/java/store/dto/PromotionBuilder.java
@@ -1,20 +1,27 @@
 package store.dto;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-public record PromotionBuilder(String name, int buyQuantity, LocalDateTime startDate, LocalDateTime endDate) {
+public record PromotionBuilder(String name, int buyQuantity, LocalDate startDate, LocalDate endDate) {
     private static final int NAME_COLUMN = 0;
     private static final int BUY_QUANTITY_COLUMN = 1;
     private static final int START_DATE_COLUMN = 3;
     private static final int END_DATE_COLUMN = 4;
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
 
     public static PromotionBuilder from(List<String> fields) {
         String name = fields.get(NAME_COLUMN);
         int buyQuantity = Integer.parseInt(fields.get(BUY_QUANTITY_COLUMN));
-        LocalDateTime startDate = LocalDateTime.parse(fields.get(START_DATE_COLUMN));
-        LocalDateTime endDate = LocalDateTime.parse(fields.get(END_DATE_COLUMN));
+        LocalDate startDate = convertToLocalDate(fields.get(START_DATE_COLUMN));
+        LocalDate endDate = convertToLocalDate(fields.get(END_DATE_COLUMN));
 
         return new PromotionBuilder(name, buyQuantity, startDate, endDate);
+    }
+
+    public static LocalDate convertToLocalDate(String dateString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        return LocalDate.parse(dateString, formatter);
     }
 }
--- src/main/java/store/model/Promotion.java
+++ src/main/java/store/model/Promotion.java
@@ -1,9 +1,9 @@
 package store.model;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import store.dto.PromotionBuilder;
 
-public record Promotion(String name, int buyQuantity, LocalDateTime startDate, LocalDateTime endDat) {
+public record Promotion(String name, int buyQuantity, LocalDate startDate, LocalDate endDat) {
     public static Promotion from(PromotionBuilder builder) {
         return new Promotion(builder.name(), builder.buyQuantity(), builder.startDate(), builder.endDate());
     }
